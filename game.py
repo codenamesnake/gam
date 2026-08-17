26e0c9dc6f47f66166a14499589285be27ff4ce2
codenamesnake <lingala.ashmith@gmail.com>
2026-08-17 23:19:43 +0530

    clock

--- game.py
+++ game.py
@@ -1,9 +1,11 @@
 import pygame
+from sys import exit
 
 pygame.init()
 
 surface= pygame.display.set_mode((1920,1800)) #width , hight
-
+pygame.display.set_caption('test001')
+clock=pygame.time.Clock()
 while True:
 
 #dislays everythin in loop
@@ -11,5 +13,7 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            exit()
 
-pygame.display.update() #refresher and updater
+    pygame.display.update() #refresher and updater
+    clock.tick(30)#how much fps
