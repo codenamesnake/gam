f3c80dea6b19bc29f62918911abb5926b356314b
codenamesnake <lingala.ashmith@gmail.com>
2026-08-17 23:05:45 +0530

    display test

--- game.py
+++ game.py
@@ -1,1 +1,15 @@
-print("test0001")
+import pygame
+
+pygame.init()
+
+surface= pygame.display.set_mode((1920,1800)) #width , hight
+
+while True:
+
+#dislays everythin in loop
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+pygame.display.update() #refresher and updater
