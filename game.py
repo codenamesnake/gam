365bdc3986bec2dc0ee4c58e76f242ce27fcda85
codenamesnake <lingala.ashmith@gmail.com>
2026-08-18 23:47:52 +0530

    kratos

--- game.py
+++ game.py
@@ -8,10 +8,30 @@ pygame.display.set_caption('test001')
 clock=pygame.time.Clock()
 font=pygame.font.Font(None, 200)
 
-surface_color= pygame.Surface((1920,1800))
+
+surface_ground=pygame.Surface((3200,1800))
+surface_color= pygame.Surface((3200,1400))
+
 surface_color.fill('slateblue')
-surface_back = pygame.image.load('/home/ashmith/Desktop/dice.png')
-text_test=font.render('DICE', True,'Red')
+
+
+surface_ground.fill('forestgreen')
+
+
+surface_back = pygame.image.load('/home/ashmith/Desktop/images/sun.png').convert() #converts into a form where pygame understands
+
+
+kratos=pygame.image.load('/home/ashmith/Desktop/images/kra.jpeg').convert()
+kratos_rect=kratos.get_rect(topleft =(875,920))
+
+
+
+
+text_test=font.render('SON', True,'black')
+
+
+skull=pygame.image.load('/home/ashmith/Desktop/images/skull.jpeg')
+skull_rect=skull.get_rect(topleft = (200,920))
 
 
 while True:
@@ -23,9 +43,23 @@ while True:
             pygame.quit()
             exit()
 
-    surface.blit(surface_color,(0,0)) #top left is origin
-    surface.blit(surface_back,(700,600))
+
+    surface.blit(surface_ground,(0,0))
+    surface.blit(surface_color,(0,0))
+    #top left is origin
+    surface.blit(surface_back,(1600,100))
+
+
+    
+    kratos_rect.x -=7
+    if kratos_rect.left <-400: kratos_rect.right=2250
+    surface.blit(kratos,kratos_rect)
+
+
     surface.blit(text_test,(0,0))
+    skull_rect.x -=5
+    if skull_rect.left <-400: skull_rect.right=2250
+    surface.blit(skull,skull_rect)
 
     pygame.display.update() #refresher and updater
     clock.tick(60)#how much fps
